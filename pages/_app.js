2305698e52eb9f34e850d199cf228dae88f00a9b
Jonviter <feterjhon17@gmail.com>
2021-05-31 14:49:16 +0700

    set app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
